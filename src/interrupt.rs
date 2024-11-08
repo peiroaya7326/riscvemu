3c5e5193a572f53da0b37b9953c148f0e87a12ca
peiroaya7326 <peiroaya7326@gmail.com>
2024-11-08 23:35:14 +0800

    Add Interrupt

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -1,2 +1,26 @@
+pub const INTERRUPT_BIT: u64 = 1 << 63;
+
 #[derive(Debug, Copy, Clone)]
-pub enum Interrupt {}
+pub enum Interrupt {
+    SupervisorSoftwareInterrupt,
+    MachineSoftwareInterrupt,
+    SupervisorTimerInterrupt,
+    MachineTimerInterrupt,
+    SupervisorExternalInterrupt,
+    MachineExternalInterrupt,
+    CounterOverflowInterrupt,
+}
+
+impl Interrupt {
+    pub fn code(&self) -> u64 {
+        match self {
+            Interrupt::SupervisorSoftwareInterrupt => 1 | INTERRUPT_BIT,
+            Interrupt::MachineSoftwareInterrupt => 3 | INTERRUPT_BIT,
+            Interrupt::SupervisorTimerInterrupt => 5 | INTERRUPT_BIT,
+            Interrupt::MachineTimerInterrupt => 7 | INTERRUPT_BIT,
+            Interrupt::SupervisorExternalInterrupt => 9 | INTERRUPT_BIT,
+            Interrupt::MachineExternalInterrupt => 11 | INTERRUPT_BIT,
+            Interrupt::CounterOverflowInterrupt => 13 | INTERRUPT_BIT,
+        }
+    }
+}
